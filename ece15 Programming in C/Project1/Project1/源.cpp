a6dd1b6638bb20e6b6994c26c43d27cba8bd7a72
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2025-09-05 00:20:21 +0800

    1220

--- ece15 Programming in C/Project1/Project1/源.cpp
+++ ece15 Programming in C/Project1/Project1/源.cpp
@@ -1,17 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	int start, end;
-	char a = 'a';
-	char b = '0';
-	float flo = 1.2;
-	double dou = 3.4;
-	char c[10] = {"abcdef"};
+void findingfactors(int number)
+{
+    for (int j = 1; j <= (number / 2); ++j) {
+        if ((number % j) == 0) {
+            printf("%-5d", j);
+        }
+    }
+    printf("\n");
 
-	scanf_s("%d",&start);
-	scanf_s("%d", &end);
-	for (int i = start; i <= end; i++) {
-		printf("%10d",i);
-	}
-	return 0;
+
+}
+
+
+
+int main()
+{
+    int num1, num2, i, midnum;
+
+    printf("please enter two integer values\n");
+    scanf_s("%d%d", &num1, &num2);
+    if (num1 > num2) {
+        midnum = num1;
+        num1 = num2;
+        num2 = midnum;
+
+    }
+
+    printf("%-10s%s\n", "Number", "Factors of this number");
+
+    for (i = num1; i <= num2; ++i) {
+        printf("%-10d", i);
+        findingfactors(i);
+
+    }
+
+
+
+
+
+
+
+    return 0;
 }
